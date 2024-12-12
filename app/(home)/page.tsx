8e6512aef2ba5bb3e726ba7727aaa62243d0f42e
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2024-12-12 17:36:53 +0900

    home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-  return <h1>I love you</h1>;
+const url = "https://billions-api.nomadcoders.workers.dev/";
+
+async function getBillionaries() {
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const billion = await getBillionaries();
+  return <div>{JSON.stringify(billion)}</div>;
 }
